45898f7b95052898951624d893527b91d24d532e
federicomartinlara1976 <federicomartinlara1976@gmail.com>
2022-06-16 08:08:09 +0000

    Remove unused function

--- src/input/UsersRobot/consumer.py
+++ src/input/UsersRobot/consumer.py
@@ -6,6 +6,6 @@ import funcionesRobot as fr
 if __name__ == '__main__':
 
     # Se queda en bucle esperando por mensajes
-    fr.insertUsersFromKafka()
+    #fr.insertUsersFromKafka()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
